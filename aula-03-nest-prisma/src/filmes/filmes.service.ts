c4a0b6b6a51d265ad4449acf5609db368bc85eff
Leoruiz197 <ruizruizleo@hotmail>
2021-12-01 22:58:24 -0300

    createPrisma

--- aula-03-nest-prisma/src/filmes/filmes.service.ts
+++ aula-03-nest-prisma/src/filmes/filmes.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { UpdateFilmeDto } from './dto/update-filme.dto';
+import { Filme, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 const lista = [];
 
 @Injectable()
 export class FilmesService {
+  constructor(private prisma: PrismaService) {}
   create(createFilmeDto: CreateFilmeDto) {
     lista.push(createFilmeDto);
     return `Add com sucesso filme: ${createFilmeDto.nome}`;
+  }
+
+  async createPrisma(createFilmeDto: CreateFilmeDto): Promise<Filme> {
+    return this.prisma.filme.create({
+      data: { ...createFilmeDto },
+    });
   }
 
   findAll() {
